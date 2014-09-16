9e33102467c7038f167e87bd7cbaa4d3b2e7b19e
baims <baimsapps@gmail.com>
2014-09-17 01:45:43 +0300

    1st page basics done for iPhone
    -With constraints that work on i5, i6 and i6+.
    -Red things to be replaced by the logo, labels and symbols.
    -I think it needs other than white background color.

---
+++ Taaref/Taaref/CategoriesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoriesTableViewCell.swift
+//  Taaref
+//
+//  Created by Bader Alrasheed on 9/16/14.
+//  Copyright (c) 2014 Baims. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Taaref/Taaref/HomeViewController.swift
+++ Taaref/Taaref/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Taaref
 //
 //  Created by Bader Alrasheed on 9/16/14.
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.scrollEnabled = false
     }
+    
+    override func viewDidLayoutSubviews() {
+        self.logoImageView.layer.cornerRadius = self.logoImageView.frame.width/2
+        
+        // size @2x: 242x242
+        // size @3x:
+        println(self.logoImageView.frame.width)
+        
+        
+        self.tableView.backgroundColor = UIColor.clearColor()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+
+// UITableViewDelegate & UITableViewDataSource
+extension HomeViewController {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PersonsSegue", forIndexPath: indexPath) as CategoriesTableViewCell
+        
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor.redColor()
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+       return indexPath.row % 2 == 0 ? 70 : 54
+    }
+}
